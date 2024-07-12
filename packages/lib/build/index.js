1257f945c9165f3a17c7bbd60bd00764077be1d2
Maria <enbrakaffe@gmail.com>
2024-07-12 11:39:58 -0300

    build: removes build directory

--- packages/lib/build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.message = void 0;
-const message = 'coffee!';
-exports.message = message;
-console.log(message);
