b54da2caa6f257e1f5e35a37d389ec2380780804
Maria <enbrakaffe@gmail.com>
2024-07-12 13:55:46 -0300

    feat: discord bot can now connect

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,2 +1,16 @@
-import { message } from '@quest-fantasy/lib';
-console.log(`${message} !!!!!`);
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('../config.json');
+
+//create a new client instance 
+const client = new Client({
+    intents: [
+	GatewayIntentBits.Guilds
+    ]
+});
+
+client.once(Events.ClientReady, (readyClient: any) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
