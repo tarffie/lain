50fad387592bc7c90bf8128b98824410e4254396
yoru <enbrakaffe@gmail.com>
2024-08-12 20:21:08 -0300

    chore: refactor Die struct

--- apps/bot/src/structs/Die.ts
+++ apps/bot/src/structs/Die.ts
@@ -1,94 +1,69 @@
 import { assert } from '../utils/assert.js'
 import { Modifier } from '../interfaces/Modifier.js';
-//import { Modifier } from '../interfaces/Modifier.ts'
+
 /**
  * our dices structs so we have less problems later when trying to implement stuff to work with dices
  */
+
 class Die {
   sides: number;
-  qty: number = 1;
+  quantity: number;
   modifier: Modifier;
   /**
-   * @param {number} sides The number of sides the die has e.g : 6, 12, 20
-   * @param {number} [qty=1] the number of dice to roll e.g : 1, 2, 4
-   * @param {number[]} Modifier[] the modifiers that affect your die
-   * @param {number} [min=1]  minimum possible roll value
-   * @param {number} [max=null] The maximum possible roll value. Defaults to number of 'sides'
+   * @param {number} sides the number of sides the die has e.g : 6, 12, 20
+   * @param {Modifier} modifier The modifiers that affect the roll.
+   * @param {number} [quantity=1] the number of dice to roll e.g : 1, 2, 4
    *
-   * @throws {RequiredArgumentError} sides is required
-   * @throws {TypeError} qty must be a positive integer, and modifiers must be valid.
+   * @throws {Error} If `sides` is undefined or if `qty` is not a positive integer
    */
   constructor(
     sides: number,
-    qty: number,
-    { mod, advantage, disadvantage }: Modifier,
+    quantity: number,
+    modifier: Modifier,
   ) {
-    try {
-      this.sides = sides;
-      this.modifier = {
-        mod,
-        advantage,
-        disadvantage,
-      };
-      this.qty = qty;
-    } catch (e: unknown) {
-      const error = e as Error;
-      console.error(`error found: "${error}": "${error.message}"`);
-      if (sides === undefined) {
-        throw new Error("'sides' is required");
-      } else {
-        throw new Error('qty must be a positive integer');
-      }
-    }
+    assert(sides > 0, "'sides' is required and must be a positive integer.")
+    assert(quantity > 0, "'quantity' is required and must be a positive integer.")
+    this.sides = sides;
+    this.modifier = modifier;
+    this.quantity = quantity;
   }
 
 
-  //{die.advantage, disadvantage, die.mod}
-  async roll(
-    sides: number,
-    {advantage, disadvantage, mod}: Modifier,
-  ): Promise<number> {
-    if (!(advantage || disadvantage)) {
-      let result = Math.floor(Math.random() * sides + Math.random() * sides);
-      if (result > sides) result = sides;
-      console.log(`you rolled ${result} with ${mod} as mod`);
-      return result + mod;
-    } else {
-      const results: number[] = [];
-      for (;;) {
-        if (results.length == 2) break;
-        const num = Math.floor(Math.random() * sides + Math.random() * sides);
-        num <= sides ? results.push(num) : null;
-      }
-      // consider using map you beautiful bastard
-      results.sort((a: number, b: number) => b - a);
-      if (advantage) {
-        console.log(
-          `you rolled ${results[0]} (advantage) with ${mod} as mod, the lesser die was ${results[1]}`,
-        );
-        return results[0] + mod;
-      } else {
-        console.log(
-          `you rolled ${results[1]} (disadvantage) with ${mod} as mod, the bigger die was ${results[0]}`,
-        );
-        return results[1] + mod;
-      }
-    }
-  }
   /**
-   * @param {Die} die
-   * @return {Promise<number[]>} roll
+   * Rolls the dice and returns a Promise<number> the number that was chosen
+   * @param {number} sides - the number of sides the die has.
+   * @param {Modifier} modifier - the number of sides the die has.
+   * @returns {Promise<number>} The result of the roll, including modifiers.
    */
-  async rollDice(die: Die): Promise<number> {
-    const results: number[] = [];
-    for (let i = 0; i < die.qty; ++i) {
-      results.push(
-        await this.roll(
-          die.sides,
-          die.modifier 
-        ),
-      );
+
+  roll(sides: number, modifier: Modifier): number {
+    // rolls the dice without thinking about modifiers
+    const rollDice = () => Math.floor(Math.random() * sides);
+
+    if (!(modifier.advantage || modifier.disadvantage)) {
+      const result = rollDice();
+      return result + modifier.mod;
+    } else {
+      const results = [rollDice(),rollDice()]; 
+
+      results.sort((a: number, b: number) => b - a);
+
+      if (modifier.advantage) {
+        return results[0] + modifier.mod;
+      } else {
+        return results[1] + modifier.mod;
+      }
     }
+  }
+
+  /**
+   * Rolls the dice multiple times
+   * @param {Die} die
+   * @return {number[]} roll
+   */
+
+  async rollDice(die: Die): Promise<number> {
+    const results = new Array(die.quantity).fill(0).map(() => this.roll(die.sides, die.modifier));
     const final = results.reduce((x, y) => x + y)
     return final
   }
