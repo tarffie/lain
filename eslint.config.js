d17c99dd73397413bfb0b720e80f254a4463a970
Emilia Alves <enbrakaffe@gmail.com>
2024-08-16 01:31:52 -0300

    refactor: move bot to root directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,11 @@
-import eslint from '@eslint/js';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-//export default [
-//  { files: ['**/*.{js,mjs,cjs,ts}'] },
-//  { languageOptions: { globals: globals.browser } },
-//  pluginJs.configs.recommended,
-//  eslintPluginPrettierRecommended,
-//  eslint.configs.recommended,
-//  ...tseslint.configs.recommended,
-//];
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-);
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  { ignores: ['build/'] },
+];
--- packages/lib/src/index.ts
+++
@@ -1,2 +0,0 @@
-const message: string = 'coffee!';
-export {message};
